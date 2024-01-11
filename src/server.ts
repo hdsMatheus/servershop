31abcef0fa3beca4965b135570bc4e43ccdb49a4
matheus <no9946191@gmail.com>
2024-01-11 14:00:27 -0300

    cors server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
 import { prisma } from "./lib/prisma";
 const app = fastify();
+import cors from "@fastify/cors";
+
+app.register(cors, {
+  origin: "*",
+  methods: ["POST", "GET", "PUT"],
+});
 
 app.get("/oi", async () => {
   const jogos = await prisma.game.findMany();
